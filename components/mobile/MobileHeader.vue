0f60f52cb3b23bb3225a58ec51021cd8d16b6289
ur4n0 <ur4n0@ur4n0.ur4n0planet>
2023-02-06 15:59:05 -0300

    refactor(keep it simple): deleting the latest commit changes
    because this is bruh, I will try to keep this simple

--- components/mobile/MobileHeader.vue
+++ components/mobile/MobileHeader.vue
@@ -1,73 +1,18 @@
 <script>
 export default {
-  props: {
-    currentTab: {
-      type: String,
-      default: 'MobileContentHome',
-    },
-  },
-  emits: {
-    changeTab(tabName) {
-      if (tabName != null) {
-        return true
-      } else {
-        return false
-      }
-    },
-  },
   data() {
     return {
-      showNav: false,
-      tabs: [
-        'MobileContentHome',
-        'MobileContentBackend',
-        'MobileContentFrontend',
-        'MobileContentContact',
-      ],
     }
-  },
-  methods: {
-    changeTabOnClick(tabName) {
-      // console.log(`changed tab to ${tabName}`)
-      this.$emit('changeTab', tabName)
-    },
   },
 }
 </script>
 
 <template>
-  <!-- <header> -->
   <div class="container">
     <div class="logo">
       <h1>Matheus Fernandes</h1>
     </div>
-
-    <div v-show="!showNav" class="menuBtn" @click="showNav = !showNav">
-      <img src="/icons/menu.png" alt="button to open menu" />
-    </div>
-
-    <nav v-show="showNav">
-      <div class="menuBtn" @click="showNav = !showNav">
-        <img src="/icons/closebtn.svg" alt="button to close menu" />
-      </div>
-
-      <ul>
-        <li v-for="tab in tabs" :key="tab">
-          <button
-            :class="{ active: currentTab === tab }"
-            @click="changeTabOnClick(tab)"
-          >
-            {{ tab }}
-          </button>
-        </li>
-        <!-- <li :class="{ active: home }">Home</li>
-        <li :class="{ active: backend }"> Backend Skills </li>
-        <li :class="{ active: frontend }">Frontend Skills</li>
-        <li :class="{ active: contact }"> Contact </li> -->
-      </ul>
-    </nav>
   </div>
-  <!-- < /header> -->
 </template>
 
 <style lang="scss" scoped>
@@ -81,45 +26,6 @@ export default {
   .logo,
   .logo h1 {
     display: inline;
-  }
-
-  .menuBtn {
-    width: 24px;
-    height: 26px;
-
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
-
-  nav {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    position: absolute;
-    right: 20px;
-    top: 20px;
-
-    background-color: $background-default-color;
-    width: 30%;
-    height: 30vh;
-    transition-delay: 3s;
-
-    .menuBtn {
-      align-self: flex-end;
-    }
-
-    ul {
-      height: calc(100% - 16px);
-      margin-left: 14px;
-
-      display: flex;
-      flex-direction: column;
-      justify-content: space-evenly;
-      align-items: flex-start;
-    }
   }
 }
 </style>
--- components/mobile/MobileView.vue
+++ components/mobile/MobileView.vue
@@ -1,78 +1,13 @@
 <script>
-import MobileContentHome from './content/MobileContentHome.vue'
-import MobileContentBackend from './content/MobileContentBackend.vue'
-import MobileContentFrontend from './content/MobileContentFrontend.vue'
-import MobileContentContact from './content/MobileContentContact.vue'
-
 export default {
-  components: {
-    MobileContentHome,
-    MobileContentBackend,
-    MobileContentFrontend,
-    MobileContentContact,
-  },
   data() {
     return {
-      currentTab: 'MobileContentHome',
-      tabs: [
-        'MobileContentHome',
-        'MobileContentBackend',
-        'MobileContentFrontend',
-        'MobileContentContact',
-      ],
-      touch: {
-        startX: 0,
-        endX: 0,
-      },
+      
     }
   },
   mounted() {
-    this.$nextTick(() => {
-      window.addEventListener('scroll', this.onScroll)
-    })
-    this.$el.addEventListener('touchstart', (event) => this.onTouchStart(event))
-    this.$el.addEventListener('touchmove', (event) => this.onTouchMove(event))
-    this.$el.addEventListener('touchend', () => this.onTouchEnd())
   },
   methods: {
-    changeTab(tabName) {
-      this.currentTab = tabName
-    },
-    onTouchStart(event) {
-      this.touch.startX = event.touches[0].clientX
-      this.touch.endX = 0
-    },
-    onTouchMove(event) {
-      this.touch.endX = event.touches[0].clientX
-    },
-    onTouchEnd() {
-      if (
-        Math.abs(this.touch.endX - this.touch.startX) > 100 &&
-        this.touch.endX !== 0
-      ) {
-        // console.log(`this.touch.endX ${this.touch.endX}`)
-        // console.log(`this.touch.startX ${this.touch.startX}`)
-        if (this.touch.endX < this.touch.startX) {
-          // console.log("next")
-          this.switchToNextTab()
-        } else {
-          // console.log("previus")
-          this.switchToPreviusTab()
-        }
-      }
-    },
-    switchToNextTab() {
-      const index = this.tabs.indexOf(this.currentTab)
-      if (index < this.tabs.length - 1) {
-        this.currentTab = this.tabs[index + 1]
-      }
-    },
-    switchToPreviusTab() {
-      const index = this.tabs.indexOf(this.currentTab)
-      if (index !== 0) {
-        this.currentTab = this.tabs[index - 1]
-      }
-    },
   },
 }
 </script>
@@ -80,11 +15,15 @@ export default {
 <template>
   <div>
     <header>
-      <MobileHeader :current-tab="currentTab" @change-tab="changeTab" />
+      <MobileHeader/>
     </header>
 
     <main>
-      <component :is="currentTab"></component>
+      <MobileContentHome/>
+      <MobileContentFrontend/>
+      <MobileContentBackend/>
+      <MobileContentPortifolio/>
+      <MobileContentContact/>
     </main>
 
     <footer>

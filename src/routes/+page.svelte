6907ed57ebb5c8964eb4e410887663f07d45764c
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-08-31 17:41:59 -0300

    WIP(footer): trying to do social media links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<main>
+	<section id="im">
+		<div id="ImMatheus">
+			<h1>Hello friend, I'm Matheus :)</h1>
+			<h2>A undergraduated computer engineer (in Brazil it's a future Uber)</h2>
+			<h1>I'm a backend developer</h1>
+		</div>
+	</section>
+	<section>
+		<h1>Here's some of my work</h1>
+	</section>
+</main>
+
+<footer>
+	<div>
+		<h1>How find me</h1>
+		<div id="socialmediacontainer">
+			<a target="blank" href="https://www.twitch.tv/daisychaincosplay"
+				><i class="fab fa-twitch" /></a
+			>
+			<a target="blank" href="https://www.youtube.com/channel/UC-gCUMK_EGUqJ7P9VA7BJmQ"
+				><i class="fab fa-youtube" /></a
+			>
+			<a target="blank" href="https://twitter.com/codingCommander"><i class="fab fa-twitter" /></a>
+			<a target="blank" href="https://www.facebook.com/codingcommanders/"
+				><i class="fab fa-facebook" /></a
+			>
+			<a target="blank" href="https://www.instagram.com/codingcommanders"
+				><i class="fab fa-instagram" /></a
+			>
+			<a target="blank" href="https://social.tchncs.de/@codingcommanders"
+				><i class="fab fa-mastodon" /></a
+			>
+			<a target="blank" href="https://discordapp.com/invite/AeUvu7v">
+				<i class="fab fa-discord" /></a
+			>
+		</div>
+	</div>
+</footer>
+
+<style lang="scss">
+	main {
+		padding: 30px;
+	}
+	#ImMatheus {
+		width: 100vw;
+		align-content: center;
+		h2 {
+			font-size: 1em;
+		}
+	}
+	footer {
+		h1 {
+			font-size: 1em;
+		}
+		#socialmediacontainer {
+			display: flex;
+            width: 200px;
+            justify-content: space-evenly;
+		}
+	}
+</style>
